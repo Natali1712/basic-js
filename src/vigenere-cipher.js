7a02549c9e869f5341544efe49765f1a3d3cb92f
Natali <93080702+Natali1712@users.noreply.github.com>
2022-05-01 20:16:10 +0300

    implement VigenereCipheringMachine function

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,44 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+
+  constructor(mode) {
+    this.mode = mode || mode === undefined
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(line, key) {
+    if(!line || !key) throw new Error("Incorrect arguments!");
+    line = line.toUpperCase().split('');
+    key = key.toUpperCase().repeat(13).split('');
+    
+    for(let i =0;i<line.length;i++){
+      if(this.alphabet.includes(line[i])){
+       line[i] = this.alphabet[((this.alphabet.indexOf(line[i])+this.alphabet.indexOf(key[i]))%26)]
+      }else{
+        key.splice(i,0,' ')
+      }
+    }
+    
+    if(!this.mode) line.reverse();
+    return line.join('');
+  }
+
+  decrypt(line, key) {
+    if(!line || !key) throw new Error("Incorrect arguments!");
+    let r = line.toUpperCase().split('');
+    key = key.toUpperCase().repeat(13).split('');
+
+    for(let i =0;i<r.length;i++){
+      if(this.alphabet.includes(r[i])){
+        r[i] = this.alphabet[(this.alphabet.indexOf(r[i])+26-this.alphabet.indexOf(key[i]))%26]
+      }else{
+        key.splice(i,0,' ')
+      }
+    }
+
+    if(!this.mode) r.reverse();
+    return r.join('');
   }
 }
 
